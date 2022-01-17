5ed026f2365bc614e22c7225655f15cb358d94a5
Vladimir <mikulskiyV@gmail.com>
2022-01-17 19:34:46 +0600

    Верстка игровой страницы

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -3,7 +3,93 @@ import "./Game.sass";
 
 const Game: FC = () => {
     return(
-        <div>Game</div>
+        <section className="game">
+            <div className="game__info">
+                <div className="game__counter">Отсчет</div>
+                <div className="game__score">Количество сделанных ходов</div>
+            </div>
+            
+            <div className="game__list">
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front game__card-front--rotate">
+                            <div className="game__icon-barcode"></div>
+                        </div>
+                        <div className="game__card-back game__card-back--rotate">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                <div className="game__card">
+                        <div className="game__card-front">
+                            <div className="game__icon-barcode"></div>
+                        </div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">
+                            <div className="game__icon-barcode"></div>
+                        </div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+                <div className="game__item">
+                    <div className="game__card">
+                        <div className="game__card-front">front</div>
+                        <div className="game__card-back">back</div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 };
 
